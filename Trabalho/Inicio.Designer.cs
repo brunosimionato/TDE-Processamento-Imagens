f5984b4e9b87f7d7a4a8ba112b300328e20e1d43
brunosimionato <101061@aluno.uricer.edu.br>
2024-03-27 14:59:32 -0300

    Add project files.

---
+++ Trabalho/Inicio.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Trabalho
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inicio));
+            this.imgA = new System.Windows.Forms.PictureBox();
+            this.imgB = new System.Windows.Forms.PictureBox();
+            this.btAbrirA = new System.Windows.Forms.Button();
+            this.imgResultado = new System.Windows.Forms.PictureBox();
+            this.btAbrirB = new System.Windows.Forms.Button();
+            this.negativarBT = new System.Windows.Forms.Button();
+            this.cinzaBT = new System.Windows.Forms.Button();
+            this.somaBT = new System.Windows.Forms.Button();
+            this.gpA = new System.Windows.Forms.GroupBox();
+            this.gpB = new System.Windows.Forms.GroupBox();
+            this.gpResultado = new System.Windows.Forms.GroupBox();
+            this.btSalavar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgResultado)).BeginInit();
+            this.gpA.SuspendLayout();
+            this.gpB.SuspendLayout();
+            this.gpResultado.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imgA
+            // 
+            this.imgA.Location = new System.Drawing.Point(11, 24);
+            this.imgA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgA.Name = "imgA";
+            this.imgA.Size = new System.Drawing.Size(292, 290);
+            this.imgA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgA.TabIndex = 0;
+            this.imgA.TabStop = false;
+            // 
+            // imgB
+            // 
+            this.imgB.Location = new System.Drawing.Point(12, 24);
+            this.imgB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgB.Name = "imgB";
+            this.imgB.Size = new System.Drawing.Size(292, 290);
+            this.imgB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgB.TabIndex = 1;
+            this.imgB.TabStop = false;
+            // 
+            // btAbrirA
+            // 
+            this.btAbrirA.Location = new System.Drawing.Point(11, 329);
+            this.btAbrirA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btAbrirA.Name = "btAbrirA";
+            this.btAbrirA.Size = new System.Drawing.Size(292, 35);
+            this.btAbrirA.TabIndex = 2;
+            this.btAbrirA.Text = "Abrir A";
+            this.btAbrirA.UseVisualStyleBackColor = true;
+            this.btAbrirA.Click += new System.EventHandler(this.abrirBT1_Click);
+            // 
+            // imgResultado
+            // 
+            this.imgResultado.Location = new System.Drawing.Point(11, 16);
+            this.imgResultado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.imgResultado.Name = "imgResultado";
+            this.imgResultado.Size = new System.Drawing.Size(292, 290);
+            this.imgResultado.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgResultado.TabIndex = 3;
+            this.imgResultado.TabStop = false;
+            // 
+            // btAbrirB
+            // 
+            this.btAbrirB.Location = new System.Drawing.Point(12, 329);
+            this.btAbrirB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btAbrirB.Name = "btAbrirB";
+            this.btAbrirB.Size = new System.Drawing.Size(292, 35);
+            this.btAbrirB.TabIndex = 4;
+            this.btAbrirB.Text = "Abrir B";
+            this.btAbrirB.UseVisualStyleBackColor = true;
+            this.btAbrirB.Click += new System.EventHandler(this.abrir2BT_Click);
+            // 
+            // negativarBT
+            // 
+            this.negativarBT.Location = new System.Drawing.Point(712, 105);
+            this.negativarBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.negativarBT.Name = "negativarBT";
+            this.negativarBT.Size = new System.Drawing.Size(94, 35);
+            this.negativarBT.TabIndex = 5;
+            this.negativarBT.Text = "Negativar";
+            this.negativarBT.UseVisualStyleBackColor = true;
+            this.negativarBT.Click += new System.EventHandler(this.negativarBT_Click);
+            // 
+            // cinzaBT
+            // 
+            this.cinzaBT.Location = new System.Drawing.Point(712, 219);
+            this.cinzaBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cinzaBT.Name = "cinzaBT";
+            this.cinzaBT.Size = new System.Drawing.Size(94, 35);
+            this.cinzaBT.TabIndex = 6;
+            this.cinzaBT.Text = "Escala de Cinza";
+            this.cinzaBT.UseVisualStyleBackColor = true;
+            this.cinzaBT.Click += new System.EventHandler(this.cinzaBT_Click);
+            // 
+            // somaBT
+            // 
+            this.somaBT.Location = new System.Drawing.Point(712, 284);
+            this.somaBT.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.somaBT.Name = "somaBT";
+            this.somaBT.Size = new System.Drawing.Size(94, 35);
+            this.somaBT.TabIndex = 7;
+            this.somaBT.Text = "A + B";
+            this.somaBT.UseVisualStyleBackColor = true;
+            this.somaBT.Click += new System.EventHandler(this.somaBT_Click);
+            // 
+            // gpA
+            // 
+            this.gpA.Controls.Add(this.btAbrirA);
+            this.gpA.Controls.Add(this.imgA);
+            this.gpA.Location = new System.Drawing.Point(21, 28);
+            this.gpA.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpA.Name = "gpA";
+            this.gpA.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpA.Size = new System.Drawing.Size(314, 375);
+            this.gpA.TabIndex = 8;
+            this.gpA.TabStop = false;
+            this.gpA.Text = "Imagem A";
+            this.gpA.Enter += new System.EventHandler(this.gpA_Enter);
+            // 
+            // gpB
+            // 
+            this.gpB.Controls.Add(this.btAbrirB);
+            this.gpB.Controls.Add(this.imgB);
+            this.gpB.Location = new System.Drawing.Point(361, 28);
+            this.gpB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpB.Name = "gpB";
+            this.gpB.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpB.Size = new System.Drawing.Size(314, 375);
+            this.gpB.TabIndex = 9;
+            this.gpB.TabStop = false;
+            this.gpB.Text = "Imagem B";
+            // 
+            // gpResultado
+            // 
+            this.gpResultado.Controls.Add(this.btSalavar);
+            this.gpResultado.Controls.Add(this.imgResultado);
+            this.gpResultado.Location = new System.Drawing.Point(1085, 28);
+            this.gpResultado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpResultado.Name = "gpResultado";
+            this.gpResultado.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gpResultado.Size = new System.Drawing.Size(314, 375);
+            this.gpResultado.TabIndex = 10;
+            this.gpResultado.TabStop = false;
+            this.gpResultado.Text = "Resultado Final";
+            // 
+            // btSalavar
+            // 
+            this.btSalavar.Location = new System.Drawing.Point(11, 325);
+            this.btSalavar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btSalavar.Name = "btSalavar";
+            this.btSalavar.Size = new System.Drawing.Size(292, 35);
+            this.btSalavar.TabIndex = 5;
+            this.btSalavar.Text = "Salvar";
+            this.btSalavar.UseVisualStyleBackColor = true;
+            this.btSalavar.Click += new System.EventHandler(this.btSalavar_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1426, 839);
+            this.Controls.Add(this.gpResultado);
+            this.Controls.Add(this.gpB);
+            this.Controls.Add(this.gpA);
+            this.Controls.Add(this.somaBT);
+            this.Controls.Add(this.cinzaBT);
+            this.Controls.Add(this.negativarBT);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Inicio";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversor de Imagens";
+            ((System.ComponentModel.ISupportInitialize)(this.imgA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgResultado)).EndInit();
+            this.gpA.ResumeLayout(false);
+            this.gpB.ResumeLayout(false);
+            this.gpResultado.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgA;
+        private System.Windows.Forms.PictureBox imgB;
+        private System.Windows.Forms.Button btAbrirA;
+        private System.Windows.Forms.PictureBox imgResultado;
+        private System.Windows.Forms.Button btAbrirB;
+        private System.Windows.Forms.Button negativarBT;
+        private System.Windows.Forms.Button cinzaBT;
+        private System.Windows.Forms.Button somaBT;
+        private System.Windows.Forms.GroupBox gpA;
+        private System.Windows.Forms.GroupBox gpB;
+        private System.Windows.Forms.GroupBox gpResultado;
+        private System.Windows.Forms.Button btSalavar;
+    }
+}
+
---
+++ Trabalho/Inicio.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Trabalho
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+
+        }
+
+
+
+
+        // BOTÃO ABRIR IMAGEM 1
+        private void abrirBT1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Selecionar Imagem";
+            openFileDialog1.Filter = "Arquivos de Imagem|*.jpg;*.jpeg;*.png;*.gif|Todos os arquivos|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // EXIBE A IMAGEM NO PICTURE BOX
+                    imgA.Image = new System.Drawing.Bitmap(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    // MENSAGEM DE ERRO CASO A IMAGEM NÃO FOR CARREGADA
+                    MessageBox.Show("Erro ao carregar a imagem: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        // BOTÃO ABRIR IMAGEM 2
+        private void abrir2BT_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog2 = new OpenFileDialog();
+
+            openFileDialog2.Title = "Selecionar Imagem";
+            openFileDialog2.Filter = "Arquivos de Imagem|*.jpg;*.jpeg;*.png;*.gif|Todos os arquivos|*.*";
+            openFileDialog2.FilterIndex = 1;
+            openFileDialog2.RestoreDirectory = true;
+
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // EXIBE A IMAGEM NO PICTURE BOX
+                    imgB.Image = new System.Drawing.Bitmap(openFileDialog2.FileName);
+                }
+                catch (Exception ex)
+                {
+                    // MENSAGEM DE ERRO CASO A IMAGEM NÃO FOR CARREGADA
+                    MessageBox.Show("Erro ao carregar a imagem: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        // BOTÃO SALVAR IMAGEM
+        private void btSalavar_Click(object sender, EventArgs e)
+        {
+            // VERIFICA SE TEM UMA IMAGEM NO PICTURE BOX
+            if (imgResultado.Image == null)
+            {
+                MessageBox.Show("Não há imagem para salvar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // ABRE DIALOGO PARA SALVAMENTO
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Salvar Imagem";
+            saveFileDialog.Filter = "Arquivos de Imagem|*.jpg;*.jpeg;*.png;*.gif;*.bmp|Todos os arquivos|*.*";
+            saveFileDialog.DefaultExt = "jpg"; // FORMATO PADRÃO
+            saveFileDialog.AddExtension = true;
+
+            // AO CLICAR EM SALVAR DENTRO DO DIALOGO, A IMAGEM É SALVA
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // OBTÉM A IMAGEM DA PICTURE BOX
+                    Bitmap imagem = new Bitmap(imgResultado.Image);
+
+                    // SALVA A IMAGEM NO DISCO
+                    imagem.Save(saveFileDialog.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao salvar a imagem: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void negativarBT_Click(object sender, EventArgs e)
+        {
+            // Verifica se há uma imagem no PictureBox original
+            if (imgA.Image == null)
+            {
+                MessageBox.Show("Por favor, carregue uma imagem antes de negativar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Cria uma cópia da imagem original para negativar
+            Bitmap imagemOriginal = new Bitmap(imgA.Image);
+
+            // Itera sobre cada pixel na imagem e inverte suas cores
+            for (int x = 0; x < imagemOriginal.Width; x++)
+            {
+                for (int y = 0; y < imagemOriginal.Height; y++)
+                {
+                    Color corOriginal = imagemOriginal.GetPixel(x, y);
+                    Color corNegativa = Color.FromArgb(255 - corOriginal.R, 255 - corOriginal.G, 255 - corOriginal.B);
+                    imagemOriginal.SetPixel(x, y, corNegativa);
+                }
+            }
+
+            // Exibe a imagem negativada no PictureBoxResultado
+            imgResultado.SizeMode = PictureBoxSizeMode.Zoom; // Ajuste para exibição
+            imgResultado.Image = imagemOriginal;
+        }
+
+        private void cinzaBT_Click(object sender, EventArgs e)
+        {
+            // Verifica se há uma imagem no PictureBox original
+            if (imgA.Image == null)
+            {
+                MessageBox.Show("Por favor, carregue uma imagem antes de converter para escala de cinza.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Cria uma cópia da imagem original para converter para escala de cinza
+            Bitmap imagemOriginal = new Bitmap(imgA.Image);
+
+            // Itera sobre cada pixel na imagem e calcula a média dos valores de intensidade RGB
+            for (int x = 0; x < imagemOriginal.Width; x++)
+            {
+                for (int y = 0; y < imagemOriginal.Height; y++)
+                {
+                    Color corOriginal = imagemOriginal.GetPixel(x, y);
+                    int media = (corOriginal.R + corOriginal.G + corOriginal.B) / 3; // Calcula a média dos valores RGB
+                    Color corCinza = Color.FromArgb(media, media, media); // Cria uma cor em escala de cinza com a média calculada
+                    imagemOriginal.SetPixel(x, y, corCinza); // Define o pixel na imagem com a nova cor em escala de cinza
+                }
+            }
+
+            // Exibe a imagem em escala de cinza no PictureBoxResultado
+            imgResultado.SizeMode = PictureBoxSizeMode.Zoom; // Ajuste para exibição
+            imgResultado.Image = imagemOriginal;
+        }
+
+
+
+
+        private void somaBT_Click(object sender, EventArgs e)
+        {
+            // Verifica se há imagens nos PictureBoxes
+            if (imgA.Image == null || imgB.Image == null)
+            {
+                MessageBox.Show("Por favor, carregue duas imagens antes de realizar a soma.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Normaliza as intensidades das imagens para [0, 1]
+            Bitmap imagemA = NormalizarIntensidades(new Bitmap(imgA.Image));
+            Bitmap imagemB = NormalizarIntensidades(new Bitmap(imgB.Image));
+
+            // Realiza a soma das intensidades normalizadas
+            Bitmap resultado = SomaImagens(imagemA, imagemB);
+
+            // Exibe o resultado da soma no PictureBoxResultado
+            imgResultado.SizeMode = PictureBoxSizeMode.Zoom; // Ajuste para exibição
+            imgResultado.Image = resultado;
+        }
+
+        private Bitmap NormalizarIntensidades(Bitmap imagem)
+        {
+            Bitmap imagemNormalizada = new Bitmap(imagem.Width, imagem.Height);
+
+            for (int x = 0; x < imagem.Width; x++)
+            {
+                for (int y = 0; y < imagem.Height; y++)
+                {
+                    Color pixel = imagem.GetPixel(x, y);
+                    double intensidade = (pixel.R + pixel.G + pixel.B) / 3.0; // Calcula a média das intensidades
+                    double normalizado = intensidade / 255.0; // Normaliza para o intervalo [0, 1]
+                    int valorNormalizado = (int)Math.Round(normalizado * 255); // Ajusta para o intervalo [0, 255]
+                    Color novoPixel = Color.FromArgb(valorNormalizado, valorNormalizado, valorNormalizado);
+                    imagemNormalizada.SetPixel(x, y, novoPixel);
+                }
+            }
+
+            return imagemNormalizada;
+        }
+
+        private Bitmap SomaImagens(Bitmap imagemA, Bitmap imagemB)
+        {
+            int largura = Math.Min(imagemA.Width, imagemB.Width);
+            int altura = Math.Min(imagemA.Height, imagemB.Height);
+
+            Bitmap resultado = new Bitmap(largura, altura);
+
+            for (int x = 0; x < largura; x++)
+            {
+                for (int y = 0; y < altura; y++)
+                {
+                    Color pixelA = imagemA.GetPixel(x, y);
+                    Color pixelB = imagemB.GetPixel(x, y);
+
+                    // Soma as intensidades dos pixels normalizados
+                    double soma = pixelA.R / 255.0 + pixelB.R / 255.0;
+
+                    // Ajusta para o intervalo [0, 255]
+                    int valorSoma = (int)Math.Round(soma * 255);
+
+                    // Garante que o valor esteja dentro do intervalo válido
+                    valorSoma = Math.Max(0, Math.Min(255, valorSoma));
+
+                    Color novoPixel = Color.FromArgb(valorSoma, valorSoma, valorSoma);
+                    resultado.SetPixel(x, y, novoPixel);
+                }
+            }
+
+            return resultado;
+        }
+
+        private void gpA_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
